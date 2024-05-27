10bd8ab440ec450b7774117444c3fc0c439fa81b
Roman31X <die.haa65@gmail.com>
2024-05-27 15:15:00 -0500

    Logback agregado

--- src/main/java/com/zona_fit/ZonaFitApplication.java
+++ src/main/java/com/zona_fit/ZonaFitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZonaFitApplication {
- int num;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZonaFitApplication.class, args);
 	}
